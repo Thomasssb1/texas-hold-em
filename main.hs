6a9034cbeea585dae366a480d6047172388e5bf6
Thomas Beer <thomas.beer04@outlook.com>
2024-12-01 20:58:31 +0000

    Implement evaluation of hand

--- main.hs
+++ main.hs
@@ -7,7 +7,7 @@ data Suit = Spades | Hearts | Diamonds | Clubs
     deriving (Show, Eq, Ord)
 
 data Value = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
-    deriving (Show, Eq, Ord)
+    deriving (Show, Eq, Ord, Enum)
 
 data Card = Card {suit :: Suit, value :: Value}
 
@@ -21,6 +21,9 @@ instance Show Card where
     show (Card suit value) = show value ++ " of " ++ show suit
 
 newtype Deck = Deck [Card] deriving (Show)
+
+data HandRank = RoyalFlush | StraightFlush | FourOfAKind | FullHouse | Flush | Straight | ThreeOfAKind | TwoPair | Pair | HighCard
+    deriving (Show, Eq, Ord)
 
 data Player = Player {name :: String, hand :: [Card], chips :: Int, dealer :: Bool} deriving Show
 
@@ -37,10 +40,32 @@ main = do
     print dealerIndex
     let (result, dealerState) = runState (assignDealer dealerIndex) initialState
     let (result, dealtState) = runState dealCards dealerState
-    print result
+    --print result
     let (Deck cards) = deck result
-    print (length cards)
-    print "2"
+    let rflush = [Card Spades Ten, Card Spades Jack, Card Spades Queen, Card Spades King, Card Spades Ace]
+    let sflush = [Card Spades Two, Card Spades Three, Card Spades Four, Card Spades Five, Card Spades Six]
+    let fourofakind = [Card Spades Two, Card Hearts Two, Card Diamonds Two, Card Clubs Two, Card Spades Ace]
+    let fullHouse = [Card Spades Two, Card Hearts Two, Card Diamonds Two, Card Clubs Ace, Card Spades Ace]
+    let flush = [Card Spades Two, Card Spades Three, Card Spades Four, Card Spades Five, Card Spades Seven]
+    let straight = [Card Spades Two, Card Hearts Three, Card Diamonds Four, Card Clubs Five, Card Spades Six]
+    let threeOfAKind = [Card Spades Two, Card Hearts Two, Card Diamonds Two, Card Clubs Three, Card Spades Four]
+    let twoPair = [Card Spades Two, Card Hearts Two, Card Diamonds Three, Card Clubs Three, Card Spades Four]
+    let pair = [Card Spades Two, Card Hearts Two, Card Diamonds Three, Card Clubs Four, Card Spades Five]
+    let highCard = [Card Spades Two, Card Hearts Three, Card Diamonds Four, Card Clubs Five, Card Spades Seven]
+    print (evaluateHand rflush)
+    print (evaluateHand sflush)
+    print (evaluateHand fourofakind)
+    print (evaluateHand fullHouse)
+    print (evaluateHand flush)
+    print (evaluateHand straight)
+    print (evaluateHand threeOfAKind)
+    print (evaluateHand twoPair)
+    print (evaluateHand pair)
+    print (evaluateHand highCard)
+    --print (hand ((activePlayers result) !! (dealerIndex - 1)))
+    --print (evaluateHand (hand ((activePlayers result) !! (dealerIndex - 1))))
+     
+    
 
 createInitialState :: [Player] -> GameState
 createInitialState players = GameState players (shuffleDeck 1) 0 [] 0 1 2
@@ -82,3 +107,34 @@ dealCards = do
         player = Player (name p) (hand p ++ take numCards deck) (chips p) (dealer p)
         (players, Deck updatedDeck) = dealCardsToPlayers ps (Deck (drop numCards deck))
         in (player : players, Deck updatedDeck)
+
+evaluateHand :: [Card] -> HandRank
+evaluateHand hand = let
+        isRoyalFlush = allSameSuit && allConsecutive && value (minimum hand) == Ten
+        isStraightFlush = allSameSuit && allConsecutive
+        isFourOfAKind = nOfAKind 4
+        isFullHouse = nOfAKind 3 && nOfAKind 2
+        isFlush = allSameSuit
+        isStraight = allConsecutive
+        isThreeOfAKind = nOfAKind 3
+        isTwoPair = getNOfAKind 2 == 2
+        isPair = getNOfAKind 2 == 1
+        allSameSuit = all (\c -> suit c == suit (head hand)) hand
+    in case () of
+        _ | isRoyalFlush -> RoyalFlush
+        _ | isStraightFlush -> StraightFlush
+        _ | isFourOfAKind -> FourOfAKind
+        _ | isFullHouse -> FullHouse
+        _ | isFlush -> Flush
+        _ | isStraight -> Straight
+        _ | isThreeOfAKind -> ThreeOfAKind
+        _ | isTwoPair -> TwoPair
+        _ | isPair -> Pair
+        _ -> HighCard
+    where
+        allConsecutive = let 
+            sortedHand = sort hand
+            in all (\(c1, c2) -> succ (value c1) == value c2) (zip sortedHand (tail sortedHand))
+        getNOfAKind n = length (filter (\ls -> length ls == n) (group (sort hand)))
+        nOfAKind n = getNOfAKind n > 0
+        
